2450134853f103682ddfc548c8c427489d7f8cb4
Samuel De Backer <sdebacker@gmail.com>
2025-11-02 18:07:42 +0100

    init

---
+++ src/Facades/Translatable.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace TypiCMS\Translatable\Facades;
+
+use Illuminate\Support\Facades\Facade;
+
+/**
+ * @see \Spatie\Translatable\Translatable
+ */
+class Translatable extends Facade
+{
+    protected static function getFacadeAccessor()
+    {
+        return 'translatable';
+    }
+}
